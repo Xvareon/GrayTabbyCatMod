10499879fcf9fcf963497b733ffb9687f1a19973
Gavs <poOdrag0n69@gmail.com>
2023-08-19 19:24:24 +0800

    Implemented the base mob with behavior

---
+++ src/main/java/github/xvareon/graytabbycatmod/client/renderer/GrayTabbyCatRenderer.java
@@ -0,0 +1,23 @@
+package github.xvareon.graytabbycatmod.client.renderer;
+
+import github.xvareon.graytabbycatmod.GrayTabbyCatMod;
+import github.xvareon.graytabbycatmod.entity.GrayTabbyCat;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.model.CatModel;
+import net.minecraft.client.model.geom.ModelLayers;
+
+public class GrayTabbyCatRenderer extends MobRenderer<GrayTabbyCat, CatModel<GrayTabbyCat>> {
+
+    public static final ResourceLocation TEXTURE = new ResourceLocation(GrayTabbyCatMod.MODID, "textures/entity/graytabbycat.png");
+
+    public GrayTabbyCatRenderer (EntityRendererProvider.Context ctx){
+        super(ctx, new CatModel<>(ctx.bakeLayer(ModelLayers.CAT)), 0.5F);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(GrayTabbyCat entity){
+        return TEXTURE;
+    }
+}
