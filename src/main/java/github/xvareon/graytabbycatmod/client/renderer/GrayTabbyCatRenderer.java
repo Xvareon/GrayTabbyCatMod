7073627508dfc1f6d74258d42d9d206845364dfa
Gavs <poOdrag0n69@gmail.com>
2023-08-21 14:23:43 +0800

    #3 Successfully rendered collar colors

--- src/main/java/github/xvareon/graytabbycatmod/client/renderer/GrayTabbyCatRenderer.java
+++ src/main/java/github/xvareon/graytabbycatmod/client/renderer/GrayTabbyCatRenderer.java
@@ -9,16 +9,38 @@ import net.minecraft.client.model.CatModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import org.jetbrains.annotations.NotNull;
 
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+
 public class GrayTabbyCatRenderer extends MobRenderer<GrayTabbyCat, CatModel<GrayTabbyCat>> {
 
     public static final ResourceLocation TEXTURE = new ResourceLocation(GrayTabbyCatMod.MODID, "textures/entity/graytabbycat.png");
+    private static final ResourceLocation COLLAR = new ResourceLocation(GrayTabbyCatMod.MODID, "textures/collar.png");
 
     public GrayTabbyCatRenderer (EntityRendererProvider.Context ctx){
-        super(ctx, new CatModel<>(ctx.bakeLayer(ModelLayers.CAT)), 0.5F);
+        super(ctx, new CatModel<>(ctx.bakeLayer(ModelLayers.CAT)), 0.4F);
+
+        addLayer(new GrayTabbyCatCollarLayer(this));
     }
 
     @Override
     public @NotNull ResourceLocation getTextureLocation(@NotNull GrayTabbyCat entity){
         return TEXTURE;
+    }
+
+    static class GrayTabbyCatCollarLayer extends RenderLayer<GrayTabbyCat, CatModel<GrayTabbyCat>> {
+
+        public GrayTabbyCatCollarLayer(RenderLayerParent<GrayTabbyCat, CatModel<GrayTabbyCat>> renderer) {
+            super(renderer);
+        }
+
+        @Override
+        public void render(@NotNull PoseStack matrix, @NotNull MultiBufferSource buffer, int light, @NotNull GrayTabbyCat entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+
+            float[] afloat = entity.getCollarColor().getTextureDiffuseColors();
+            renderColoredCutoutModel(getParentModel(), COLLAR, matrix, buffer, light, entity, afloat[0], afloat[1], afloat[2]);
+        }
     }
 }
