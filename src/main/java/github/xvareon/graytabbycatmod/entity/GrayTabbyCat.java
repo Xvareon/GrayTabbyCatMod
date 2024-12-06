47a90662b0fce5eefdd2edc94b41dfcb03e0eb15
Gavs <poOdrag0n69@gmail.com>
2024-12-06 20:44:43 +0800

    feat: Initially implemented lifting logic when attacking

--- src/main/java/github/xvareon/graytabbycatmod/entity/GrayTabbyCat.java
+++ src/main/java/github/xvareon/graytabbycatmod/entity/GrayTabbyCat.java
@@ -3,13 +3,19 @@ package github.xvareon.graytabbycatmod.entity;
 import java.util.UUID;
 
 import github.xvareon.graytabbycatmod.init.EntityInit;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import org.jetbrains.annotations.NotNull;
 import net.minecraft.server.level.ServerLevel;
@@ -26,17 +32,32 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.DyeItem;
 
+import java.util.EnumSet;
+
 public class GrayTabbyCat extends Cat {
 
     private static final EntityDataAccessor<Integer> COLLAR_COLOR = SynchedEntityData.defineId(GrayTabbyCat.class, EntityDataSerializers.INT);
 
     public GrayTabbyCat(EntityType<GrayTabbyCat> type, Level level) {
         super(type, level);
+
+        this.goalSelector.addGoal(2, new AIMeleeAttack(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this) {
+            @Override
+            public boolean canUse() {
+                return !GrayTabbyCat.this.isInSittingPose() && !GrayTabbyCat.this.isOrderedToSit() && super.canUse();
+            }
+        });
     }
 
     public GrayTabbyCat(Level level, BlockPos position) {
         super(EntityInit.GRAY_TABBY_CAT.get(), level);
         this.setPos(position.getX(), position.getY(), position.getZ());
+    }
+
+    @Override
+    public boolean causeFallDamage(float f, float g, DamageSource damageSource) {
+        return false;
     }
 
     @Override
@@ -110,5 +131,152 @@ public class GrayTabbyCat extends Cat {
         }
 
         return babycat;
+    }
+
+    static class AIMeleeAttack extends Goal {
+        protected Level world;
+        protected GrayTabbyCat attacker;
+        protected int attackTick;
+        protected double liftY = 0;
+
+        public AIMeleeAttack(GrayTabbyCat graytabbycat) {
+            this.attacker = graytabbycat;
+            this.world = graytabbycat.getCommandSenderWorld();
+            this.setFlags(EnumSet.of(Flag.MOVE, Flag.TARGET));
+        }
+
+        @Override
+        public boolean canUse() {
+            LivingEntity entitylivingbase = this.attacker.getTarget();
+
+            if(entitylivingbase == null) {
+                return false;
+            } else if(!entitylivingbase.isAlive()) {
+                return false;
+            }
+
+            return !(entitylivingbase instanceof Player) || !entitylivingbase.isInvulnerable();
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return canUse();
+        }
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void stop() {
+            LivingEntity entitylivingbase = this.attacker.getTarget();
+
+            if(entitylivingbase instanceof Player && (entitylivingbase.isSpectator() || ((Player) entitylivingbase).isCreative())) {
+                this.attacker.setTarget(null);
+            }
+        }
+
+        @Override
+        public void tick() {
+
+            LivingEntity entitylivingbase = this.attacker.getTarget();
+            if(entitylivingbase == null) {
+                return;
+            }
+
+            double targetX = entitylivingbase.getX();
+            double targetZ = entitylivingbase.getZ();
+
+            if(entitylivingbase.distanceToSqr(this.attacker) < 4096.0D) // If the distance is less than 4096 square blocks rotate towards them
+            {
+                double d1 = targetX - this.attacker.getX();
+                double d2 = targetZ - this.attacker.getZ();
+                this.attacker.setYRot(-((float) Mth.atan2(d1, d2)) * (180F / (float) Math.PI));
+                this.attacker.yBodyRot = this.attacker.getYRot();
+            }
+
+            double distanceToTarget = this.attacker.distanceToSqr(entitylivingbase.getX(), entitylivingbase.getY(),
+                    entitylivingbase.getZ());
+
+            // Reduce time till attack
+            this.attackTick--;
+
+            double reachToTarget = this.getAttackReachSqr(entitylivingbase);
+
+            // If the entity can reach its target and it's time to attack, reset
+            // the timer and attack if the entity is not grabbed
+            if(distanceToTarget <= reachToTarget && this.attackTick <= 0) {
+                this.attackTick = 20;
+                if(!entitylivingbase.hasPassenger(attacker)) {
+                    this.attacker.doHurtTarget(entitylivingbase);
+                }
+            }
+
+            // If the entity is not grabbing a target, set it to move to its target
+            if(attacker.getPassengers().isEmpty()) {
+                this.attacker.navigation.moveTo(this.attacker.navigation.createPath(entitylivingbase.blockPosition(), 0), 0.4D);
+            } else { // If the entity is grabbing a target, set it to move upwards
+                this.attacker.navigation.moveTo(this.attacker.navigation.createPath(new BlockPos((int) targetX, (int) this.liftY + 15, (int) targetZ), 0), 0.4D);
+            }
+
+            // If the entity is in range and entity is not grabbing a target and
+            // the target's height is less than 3 blocks
+            if(distanceToTarget <= reachToTarget && attacker.getPassengers().isEmpty() && entitylivingbase.getBbHeight() <= 3 && this.attackTick == 15) {
+                // Move the entity upwards to avoid being stuck in the ground
+                // this.attacker.moveTo(this.attacker.getX(), this.attacker.getY() + entitylivingbase.getBbHeight() + 40,
+                this.attacker.moveTo(this.attacker.getX(), this.attacker.getY() + entitylivingbase.getBbHeight() + 2,
+                        this.attacker.getZ(), this.attacker.getYRot(), this.attacker.getXRot());
+                // Grab the target
+                entitylivingbase.startRiding(this.attacker, true);
+                // Set liftY so entity can continue moving up from the spot
+                this.liftY = entitylivingbase.getY();
+                // Remove targets of the entity (to avoid something stupid like
+                // a skeleton shooting while being eaten by a bird)
+                if(entitylivingbase instanceof Mob) {
+                    Mob el = (Mob) entitylivingbase;
+                    el.setTarget(null);
+                    el.setLastHurtByMob(null);
+                    el.getNavigation().stop();
+                    el.setNoAi(true);
+                }
+                // Move upwards
+                this.attacker.navigation.moveTo(this.attacker.navigation.createPath(new BlockPos((int) targetX, (int) this.liftY + 15, (int) targetZ), 0), 0.4D);
+            }
+
+            // If the entity is grabbing a target and the block above is solid
+            // (stuck)
+            if(attacker.getPassengers().isEmpty() && this.attacker.getCommandSenderWorld().getBlockState(this.attacker.blockPosition().above()).isRedstoneConductor(world, this.attacker.blockPosition().above())) {
+                // Release target
+                entitylivingbase.stopRiding();
+                if(entitylivingbase instanceof Mob) {
+                    Mob el = (Mob) entitylivingbase;
+                    el.setNoAi(false);
+                }
+                // Remove target
+                this.attacker.setTarget(null);
+                // Create a random target position
+                BlockPos rPos = new BlockPos(this.attacker.getRandom().nextInt(50) - 25, this.attacker.getRandom().nextInt(40) - 20, this.attacker.getRandom().nextInt(50) - 25);
+                BlockPos pos = this.attacker.blockPosition();
+                rPos = rPos.offset(pos);
+                // Move to random target position
+                this.attacker.navigation.moveTo(this.attacker.navigation.createPath(rPos, 0), 0.4D);
+            }
+
+            // If we've about reached the target lifting point and have a target
+            // grabbed, or have completed movement, drop the entity
+            if (Math.abs(this.attacker.getY() - (this.liftY + 15)) <= 3 && attacker.getPassengers().isEmpty()) {
+                entitylivingbase.stopRiding();
+                if(entitylivingbase instanceof Mob) {
+                    Mob el = (Mob) entitylivingbase;
+                    el.setNoAi(false);
+                }
+            }
+
+        }
+
+        protected double getAttackReachSqr(LivingEntity attackTarget) {
+            return 4D + attackTarget.getBbWidth();
+        }
     }
 }
