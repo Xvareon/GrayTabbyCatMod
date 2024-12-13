ef91d2e2e850d5b2b66dfa406d51ef1b01b70692
Gavs <poOdrag0n69@gmail.com>
2024-12-14 04:21:23 +0800

    feat: Soul sand glass block now behaves more like scaffolding

--- src/main/java/github/xvareon/graytabbycatmod/block/SoulSandGlassBlock.java
+++ src/main/java/github/xvareon/graytabbycatmod/block/SoulSandGlassBlock.java
@@ -1,12 +1,40 @@
 package github.xvareon.graytabbycatmod.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.ScaffoldingBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class SoulSandGlassBlock extends ScaffoldingBlock {
 
     public SoulSandGlassBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
+        return false;
+    }
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) { }
+
+    @Override
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        return InteractionResult.PASS;
+    }
+
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return true;
     }
 
 }
