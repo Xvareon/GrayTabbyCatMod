a9f96b55c70ffe7661ec823dd90a4b7bf39d5360
Gavs <poOdrag0n69@gmail.com>
2025-03-20 23:54:35 +0800

    chore: Removed unused import

--- src/main/java/github/xvareon/graytabbycatmod/entity/Barnacle.java
+++ src/main/java/github/xvareon/graytabbycatmod/entity/Barnacle.java
@@ -18,7 +18,6 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.phys.Vec3;
